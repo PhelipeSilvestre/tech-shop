f3977630c66f4c44554c887a65eff8bd440c114d
Phelipe Silvestre <phelipe17silvestre@gmail.com>
2025-02-09 19:09:00 -0300

    First commit

---
+++ config/ecommerce.php
@@ -0,0 +1,10 @@
+return [
+    'tax' => 0.1, // 10% de imposto
+    'order_status' => [
+        'pending' => 'Pendente',
+        'paid' => 'Pago',
+        'shipped' => 'Enviado',
+        'delivered' => 'Entregue',
+        'canceled' => 'Cancelado',
+    ],
+];
